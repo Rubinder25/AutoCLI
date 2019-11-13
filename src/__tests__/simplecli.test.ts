d5997bf21c5a0947a6e264abca0605c18416c461
Rubinder Singh <rsingh@networkten.com.au>
2019-11-13 13:16:56 +1100

    added tests

---
+++ src/__tests__/simplecli.test.ts
@@ -0,0 +1,13 @@
+import {execSync} from 'child_process';
+
+function cmd(command: string): string {
+  let out = '';
+  try {
+    out = String(execSync(command));
+  } catch (e) {
+    console.log('An error had occurred while processing the command');
+  }
+  return out;
+}
+
+
