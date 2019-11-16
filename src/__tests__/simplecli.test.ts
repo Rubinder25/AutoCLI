0add45350b2b0e56045ea066cf930fd1b648c45d
Rubinder Singh <mail2rubinder@gmail.com>
2019-11-16 19:15:35 +1100

    test cases passing

--- src/__tests__/simplecli.test.ts
+++ src/__tests__/simplecli.test.ts
@@ -1,5 +1,23 @@
 import {execSync} from 'child_process';
-import flags from './flags';
+import {FlagsObjectType, FlagConfigType, ParsedResultType} from '../types';
+import {SimpleCLI} from '../simplecli';
+import {EEXIST} from 'constants';
+
+type TestFlagObjType = FlagsObjectType<'test'>;
+
+const parse = (
+  input: string[],
+  testFlagObj: TestFlagObjType,
+): [ParsedResultType<TestFlagObjType>, number[]] => {
+  const simpleCLI = new SimpleCLI('Test Program', '1.0.0');
+  let errCodes: number[] = [];
+
+  const res = simpleCLI.parse(input, testFlagObj, (err) => {
+    errCodes.push(err.code);
+  });
+
+  return [res, errCodes];
+};
 
 function cmd(command: string): string {
   let out = '';
@@ -11,3 +29,157 @@ function cmd(command: string): string {
   return out;
 }
 
+const defaultFlagConfig: FlagConfigType = {
+  alias: '-t',
+  flag: '--test',
+  description: 'test flag',
+};
+
+describe('Test- Parse Configs:', () => {
+  let testFlagObj: TestFlagObjType;
+  let res: ParsedResultType<TestFlagObjType>;
+  let errCodes: number[];
+  let inputs: string[][] = [];
+
+  test('argument:false | required:false', () => {
+    testFlagObj = {
+      test: {
+        ...defaultFlagConfig,
+      },
+    };
+
+    inputs = [];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe(undefined);
+      expect(errCodes.length).toBe(0);
+    });
+
+    testFlagObj = {
+      test: {
+        ...defaultFlagConfig,
+        argument: false,
+        required: false,
+        showAstrisk: false,
+      },
+    };
+
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe(undefined);
+      expect(errCodes.length).toBe(0);
+    });
+
+    inputs = [['-t'], ['--test']];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe('test');
+      expect(errCodes.length).toBe(0);
+    });
+  });
+
+  test('argument:false | required:true', () => {
+    testFlagObj = {
+      test: {
+        ...defaultFlagConfig,
+        required: true,
+      },
+    };
+
+    inputs = [];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe(undefined);
+      expect(errCodes.length).toBe(1);
+      expect(errCodes[0]).toBe(102);
+    });
+
+    inputs = [['-t'], ['--test']];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe('test');
+      expect(errCodes.length).toBe(0);
+    });
+
+    inputs = [
+      ['-t', 'testArg'],
+      ['--test', 'testArg'],
+    ];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe('test');
+      expect(errCodes.length).toBe(1);
+      expect(errCodes[0]).toBe(103);
+    });
+  });
+
+  test('argument:true | required:false', () => {
+    testFlagObj = {
+      test: {
+        ...defaultFlagConfig,
+        argument: true,
+      },
+    };
+
+    inputs = [];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe(undefined);
+      expect(errCodes.length).toBe(0);
+    });
+
+    inputs = [['-t'], ['--test']];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe(undefined);
+      expect(errCodes.length).toBe(1);
+      expect(errCodes[0]).toBe(101);
+    });
+
+    inputs = [
+      ['-t', 'testArg'],
+      ['--test', 'testArg'],
+    ];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe('testArg');
+      expect(errCodes.length).toBe(0);
+    });
+  });
+
+  test('argument:true | required:true', () => {
+    testFlagObj = {
+      test: {
+        ...defaultFlagConfig,
+        argument: true,
+        required: true,
+      },
+    };
+
+    inputs = [];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe(undefined);
+      expect(errCodes.length).toBe(1);
+      expect(errCodes[0]).toBe(102);
+    });
+
+    inputs = [['-t'], ['--test']];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe(undefined);
+      expect(errCodes.length).toBe(1);
+      expect(errCodes[0]).toBe(101);
+    });
+
+    inputs = [
+      ['-t', 'testArg'],
+      ['--test', 'testArg'],
+    ];
+    inputs.forEach((input) => {
+      [res, errCodes] = parse(input, testFlagObj);
+      expect(res.test).toBe('testArg');
+      expect(errCodes.length).toBe(0);
+    });
+  });
+});
