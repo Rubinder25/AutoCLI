f5a24f3ef28499c55200f1b233ff10933bae188d
Rubinder Singh <mail2rubinder@gmail.com>
2019-11-26 18:42:20 +1100

    removed dev code

--- src/__tests__/cli_testers/name_version_not_provided.ts
+++ src/__tests__/cli_testers/name_version_not_provided.ts
@@ -2,6 +2,5 @@ import {SimpleCLI} from '../../simplecli';
 import flags from './flags';
 
 const program = new SimpleCLI();
-console.log(process.argv);
 
 program.parse(process.argv.slice(2), flags, () => {});
