5847f449a1a563a19c1c36bff8b2017740969340
Rubinder Singh <mail2rubinder@gmail.com>
2019-12-03 16:15:59 +1100

    stringify cli output

--- src/__tests__/cli_testers/testFlags.ts
+++ src/__tests__/cli_testers/testFlags.ts
@@ -4,6 +4,6 @@ import flags from './flags';
 const program = new SimpleCLI('test_runner', '1.0.0');
 
 program.interactive(flags).then((res) => {
-  console.log(res);
+  console.log(JSON.stringify(res));
   process.exit(0);
 });
